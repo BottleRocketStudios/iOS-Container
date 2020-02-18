f85566f01b2b405c1ed42425a3a99111d1bc0f18
Will McGinty <will.mcginty@bottlerocketstudios.com>
2020-02-18 13:37:28 -0600

    Add documentation to PostTransitionBehavior

--- Sources/Container/ContainerViewController.PostTransitionBehavior.swift
+++ Sources/Container/ContainerViewController.PostTransitionBehavior.swift
@@ -10,8 +10,14 @@ import UIKit
 extension ContainerViewController {
 
     // MARK: Subtypes
+
+    /// This type describes a function or behavior that is run after a `ContainerViewController` finishes a transition.
     public enum PostTransitionBehavior {
+
+        /// No action will be taken after each transition.
         case none
+
+        /// Describes a custom action that will be taken after each transition. The behavior is given access to the `ChildManager` as well as the currently visible `Child` at the time of execution.
         case custom((ContainerViewController.ChildManager, Child) -> Void)
         
         func execute(with childManager: ChildManager, for visible: Child) {
@@ -22,7 +28,12 @@ extension ContainerViewController {
         }
         
         // MARK: Convenience
+
+        /// This behavior will remove all `Child` objects the `ChildManager` is storing, except for the one currently visible.
         public static let removeAllNonVisibleChildren: PostTransitionBehavior = .removeAllNonVisibleChildren(exceptWithIdentifiers: [])
+
+        /// This behavior will remove all `Child` objects the `ChildManager` is storing, except for the one currently visible and any children whose identifiers have been specified.
+        /// - Parameter identifiers: A list of identifiers of `Child` objects that are excepted from removal, regardless of their visibility.
         public static func removeAllNonVisibleChildren(exceptWithIdentifiers identifiers: [ContainerViewController.Child.Identifier]) -> PostTransitionBehavior {
             return .custom { childManager, visibleChild in
                 return childManager.removeAll(where: { $0 != visibleChild && !identifiers.contains($0.identifier) })
