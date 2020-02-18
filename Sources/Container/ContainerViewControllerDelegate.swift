6fb7955f3a444b015e0c498167374c3de2edfd5e
Will McGinty <will.mcginty@bottlerocketstudios.com>
2020-02-18 13:31:31 -0600

    Add documentation to ControllerViewControllerDelegate

--- Sources/Container/ContainerViewControllerDelegate.swift
+++ Sources/Container/ContainerViewControllerDelegate.swift
@@ -8,22 +8,51 @@
 import UIKit
 
 public protocol ContainerViewControllerDelegate: class {
-    
+
+    /// Asks your delegate for the transition animator object to use when transitioning between two child view controllers.
+    /// - Parameters:
+    ///   - container: The `ContainerViewController` instance hosting the transition.
+    ///   - source: The `Child` acting as the source of the transition.
+    ///   - destination: The `Child` acting as the destination of the transition.
     func containerViewController(_ container: ContainerViewController,
                                  animationControllerForTransitionFrom source: ContainerViewController.Child,
                                  to destination: ContainerViewController.Child) -> UIViewControllerAnimatedTransitioning?
+
+    /// Asks your delegate for the transition interactor object to use when presenting a view controller.
+    /// - Parameters:
+    ///   - container: The `ContainerViewController` instance hosting the transition.
+    ///   - source: The `Child` acting as the source of the transition.
+    ///   - destination: The `Child` acting as the destination of the transition.
     func containerViewController(_ container: ContainerViewController,
                                  interactionControllerForTransitionFrom source: ContainerViewController.Child,
                                  to destination: ContainerViewController.Child) -> ContainerViewControllerInteractiveTransitioning?
-    
+
+    /// Called to let you fine tune the situations in which the container is allowed to transition.
+    /// - Parameters:
+    ///   - container: The `ContainerViewController` instance hosting the transition.
+    ///   - source: The `Child` acting as the source of the transition.
+    ///   - destination: The `Child` acting as the destination of the transition.
     func containerViewController(_ container: ContainerViewController, shouldTransitionFrom source: ContainerViewController.Child,
                                  to destination: ContainerViewController.Child) -> Bool
+
+    /// Tells the delegate that the container has just started transitioning.
+    /// - Parameters:
+    ///   - container: The `ContainerViewController` instance hosting the transition.
+    ///   - source: The `Child` acting as the source of the transition.
+    ///   - destination: The `Child` acting as the destination of the transition.
     func containerViewController(_ container: ContainerViewController, didBeginTransitioningFrom source: ContainerViewController.Child,
                                  to destination: ContainerViewController.Child)
+
+    /// Tells the delegate that the container has just finished transitioning.
+    /// - Parameters:
+    ///   - container: The `ContainerViewController` instance hosting the transition.
+    ///   - source: The `Child` acting as the source of the transition.
+    ///   - destination: The `Child` acting as the destination of the transition.
     func containerViewController(_ container: ContainerViewController, didFinishTransitioningFrom source: ContainerViewController.Child,
                                  to destination: ContainerViewController.Child)
 }
 
+// MARK: Default Implementations
 public extension ContainerViewControllerDelegate {
     
     func containerViewController(_ container: ContainerViewController,
